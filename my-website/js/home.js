83ddc2ad4399437d322c2f9a8eb4315fb26423f6
seanjhunel <jhunellastrollo@gmail.com>
2025-04-29 08:17:55 +0800

    Update home.js
    Signed-off-by: seanjhunel <jhunellastrollo@gmail.com>

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'e1f3093a1c82d15ce076f459df7568bd';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
